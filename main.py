f87a04f9fdc7752160057e324cfefe253df6fd69
danielcasto <32224738+danielcasto@users.noreply.github.com>
2021-01-30 01:11:36 -0500

    Update main.py
    Added game loop, added key events, created window with title "Swamphack VII".

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 750
+
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+pygame.display.set_caption('Swamphack VII')
+
+
+gameActive = True
+while gameActive:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameActive = False
+
+	screen.fill((0, 0, 0))
+
+	#updates display
+	pygame.display.flip()
+
+pygame.quit()
+
