47f3afbe1dca264e05544a16985304e9e45ca3c4
Michael Mendez <michael.mendez@ufl.edu>
2021-01-30 00:22:17 -0500

    api added

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+# This is written for PYTHON 3
+# Don't forget to install requests package
+import requests
+import json
 
+customerId = 'your customerId here'
+apiKey = '9e000d271e22e273a4b253ab8ee5b05d'
+
+url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
+payload = {
+  "type": "Savings",
+  "nickname": "test",
+  "rewards": 10000,
+  "balance": 10000,	
+}
+# Create a Savings Account
+response = requests.post( 
+	url, 
+	data=json.dumps(payload),
+	headers={'content-type':'application/json'},
+	)
+
+if response.status_code == 201:
+	print('account created')
